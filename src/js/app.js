b303ffae3db9a83468be0fc9604e90545bdac2c1
SuatSayharman <suat.sayharman@enuygun.com>
2019-08-04 18:58:13 +0300

    basic custom webpack-babel pipeline setup

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+// Custom Components
+import App from './index'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const config = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build/js'),
+    filename: 'output.bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
+
+module.exports = config
